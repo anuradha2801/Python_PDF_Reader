472f69f4e4598bf929cf9a83ed3e786e66a0b3c6
anuradha2801 <87013324+anuradha2801@users.noreply.github.com>
2023-05-05 21:13:43 +0530

    Update pdfreader.py

--- pdfreader.py
+++ pdfreader.py
@@ -1,12 +1,16 @@
-
+#adding required libraries
 import pyttsx3
 import PyPDF2
 
+#reading any pdf file and storing the total number of pages in it.
 book=open('C:/Users/91930/Desktop/SEPM_anuradha.pdf','rb')
 pdfReader = PyPDF2.PdfReader(book)
 pages = len(pdfReader.pages)
+
+#initializing the speaker
 speaker = pyttsx3.init()
 
+#loop for reading all the pages of the pdf with indexing zero
 for num in range(0, pages):
     page = pdfReader.pages[num] 
     txt = page.extract_text()
